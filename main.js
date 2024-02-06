c0cfd7131249e162d9427f2082aa69e9b0b2dc92
azizbek400 <aziznormurodov400@gmail.com>
2024-02-06 06:16:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// Array1   ////////////////////////////////////////////
+
+// function getInitialOdds(n) {
+//     let arr = [];
+//     for (let i = 1; i < 2 * n; i++) {
+//         if ( i % 2 === 1) {
+//             arr.push(i);
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(getInitialOdds(5));
+
+
+/////////////////////////////////////////////////////
+
+// const inverse = (n) => +n.toString().split('').reverse().join('');
+// console.log(inverse(442));
+
+// Array3    /////////////////////////////////////////////
+
+// const numbers = [10, 20, 30, 40, 50]
+// numbers.reverse()
+// console.log(numbers);
+
